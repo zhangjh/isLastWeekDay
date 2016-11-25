1eef9e2db0ead30b9da1abf4b713ce0ae6b1b0b0
zhangjh <njhxzhangjihong@126.com>
2016-11-25 14:34:19 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,9 @@ var config = {
   fromEmail: xxxx,
   subject: xxxx,
   message: xxxx,
-  xuer: xxxx,
-  xpasswd: xxxx
+  xuser: xxxx,                   //发件人
+  xpasswd: xxxx,                 //发件人密码
+  mailServer: "smtp.126.com"
 };
 
 module.exports = config;
