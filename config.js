874fa61e6e4f9b691a0abda3ced3f95b731ebe41
jihong.zjh <jihong.zjh@alibaba-inc.com>
2017-03-09 18:17:59 +0800

    修改访问方式

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var config = {
-  toEmail: xxxx,
-  fromEmail: xxxx,
-  subject: xxxx,
-  message: xxxx,
-  xuser: xxxx,                   //发件人
-  xpasswd: xxxx,                 //发件人密码
+  toEmail: "jihong.zjh@alibaba-inc.com",
+  fromEmail: "njhxzhangjihong@126.com",
+  subject: "月底了，发了工资记得定投哦~",
+  message: "定投贵在坚持，请坚持你的理财计划~",
+  xuser: "njhxzhangjihong@126.com",                   //发件人
+  xpasswd: "Wwdhqxhle=mc@!Y",                 //发件人密码
   mailServer: "smtp.126.com"
 };
 
