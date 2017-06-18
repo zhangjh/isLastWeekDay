8e420c3bc4d22f2b4011e1ce545bb7714b9ac2b9
zhangjh <njhxzhangjihong@126.com>
2017-06-18 18:21:07 +0800

    up

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 var config = {
-  toEmail: "jihong.zjh@alibaba-inc.com",
-  fromEmail: "njhxzhangjihong@126.com",
-  subject: "月底了，发了工资记得定投哦~",
-  message: "定投贵在坚持，请坚持你的理财计划~",
-  xuser: "njhxzhangjihong@126.com",                   //发件人
-  xpasswd: "Wwdhqxhle=mc@!Y",                 //发件人密码
-  mailServer: "smtp.126.com"
+  toEmail: "xxxxx",
+  fromEmail: "xxxxxx",
+  subject: "xxxxxx",
+  message: "xxxxxx",
+  xuser: "xxxxx",                   
+  xpasswd: "xxxxxx",                 
+  mailServer: "xxxxx"
 };
 
 module.exports = config;
